1989ff8a020ccde837e00a7fac8342fefd7518d2
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-05-11 18:12:22 +0600

    Implement ClickListener for Submit button

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,19 +4,23 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
-    init {
-        Log.d("GameFragment", "GameViewModel Created!")
-        getNextWord()
-    }
+
     private var wordsList:MutableList<String> = mutableListOf()
     private lateinit var currentWord:String
     private var _score = 0
     val score:Int
         get() = _score
-    private var currentWordCount = 0
+    private var _currentWordCount = 0
+    val currentWordCount:Int
+        get() = _currentWordCount
     private lateinit var _currentScrambledWord:String
     val currentScrambledWord:String
         get() = _currentScrambledWord
+
+    init {
+        Log.d("GameFragment", "GameViewModel Created!")
+        getNextWord()
+    }
 
 
     override fun onCleared() {
@@ -36,15 +40,27 @@ class GameViewModel: ViewModel() {
         }
         else{
             _currentScrambledWord = String(tempWord)
-            ++currentWordCount
+            ++_currentWordCount
             wordsList.add(currentWord)
         }
     }
 
     fun nextWord(): Boolean{
-        return if(currentWordCount < MAX_NO_OF_WORDS){
+        return if(_currentWordCount < MAX_NO_OF_WORDS){
             getNextWord()
             true
         } else false
+    }
+    private fun increaseScore(){
+        _score += SCORE_INCREASE
+    }
+    fun isUserWordCorrect(playerWord:String):Boolean{
+        if(playerWord.equals(currentWord,true)){
+            increaseScore()
+            return true
+        }
+        else{
+            return false
+        }
     }
 }
